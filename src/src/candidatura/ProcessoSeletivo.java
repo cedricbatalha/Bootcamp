bef3cf592cfaf97a5133141105f1bc1c412fa931
Cedric <cedricbatalha@gmail.com>
2024-06-24 13:53:56 -0300

    desafios

---
+++ src/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,102 @@
+package candidatura;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+public class ProcessoSeletivo {
+    public static void main (String [] args){
+
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
+
+        /*
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+         */
+
+        //selecaoCandidatos();
+        //imprimirSelecionados();
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+
+            if (continuarTentando)
+                tentativasRealizadas++;
+
+            else
+                System.out.println("Contato realizado com sucesso");
+
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println("Conseguimos contato com o candidato " + candidato + " na " + tentativasRealizadas + " da ligação.");
+        else
+            System.out.println("O candidato " + candidato + " NÃO atendeu a ligação.");
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+        System.out.println("Imprimindo a lista de candidatos indormando o indice do elemento");
+
+        for (int indice=0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de nº " + (indice+1) + " é o " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada");
+        for(String candidato: candidatos){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
+    }
+
+    static void selecaoCandidatos (){
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabrcio", "Mirela", "Daniela", "Jorge"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos [candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O Candidato " + candidato + " solicitou este valor de Salário " + salarioPretendido);
+
+            if (salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+
+        if(salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o Candidato");
+        }
+        else if (salarioBase == salarioPretendido){
+            System.out.println("Ligar para o Candidato com Contra Proposta");
+        }
+        else{
+            System.out.println("Aguardando Resultado demais Candidatos");
+        }
+    }
+}
---
+++ src/src/com/estudos/desafios/DesafioContaBancariaSwitch.java
@@ -0,0 +1,51 @@
+package com.estudos.desafio;
+
+import java.util.Scanner;
+
+public class DesafioContaBancariaSwitch {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
+                    double deposito = 0;
+                    deposito = scanner.nextDouble();
+                    saldo = deposito + saldo;
+
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 2:
+                    // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    double saque = 0;
+                    saque = scanner.nextDouble();
+
+                    if(saque <= saldo) {
+                        saldo = saldo - saque;
+                        System.out.println("Saldo atual: " + saldo);
+                    } else {
+                        System.out.println("Saldo insuficiente");
+                    }
+                    break;
+                case 3:
+                    // TODO: Exibir o saldo atual da conta.
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    continuar = false;  // Atualiza a variável de controle para encerrar o loop
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+    }
+}
